b5bad26ac408820c0f2c5be30625b704c91e58d2
Justin Nel <dogbertuk2000@gmail.com>
2018-10-01 21:25:58 +0200

    Fixed mutation error
    You should not mutate the original objects as it could pose side effects. In this instance it was a string, so it is "safe", but it is a bad habit, so best to avoid where possible. Also in this instance it is nicer to use string templates rather than string concatenation.

--- src/action-creators/action-creator.js
+++ src/action-creators/action-creator.js
@@ -5,26 +5,24 @@ import { CHOOSE_MEOWZER } from './../reducers/booking.js';
 
 export function booking(text) {
   console.log("got input:", text);
-  text = text + " more cats!";
   return {
     type: ADD_BOOKING,
-    text: text,
+    text: `${text} more cats!`,
   }
 }
 
 export function booking1(text1) {
   console.log("got input2:", text1);
-  text1 = text1 + " for a week";
   return {
     type: REMOVE_BOOKING,
-    text1: text1,
+    text1: `${text1} for a week`,
   }
 }
 
-  export function slots(catInput) {
-    console.log("got cat Input:", catInput);
-    return {
-      type: CHOOSE_MEOWZER,
-      id: catInput,
-      }
-  }
+export function slots(catInput) {
+  console.log("got cat Input:", catInput);
+  return {
+    type: CHOOSE_MEOWZER,
+    id: catInput,
+    }
+}
