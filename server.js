cf85e38239d7d3940acf73b9ff5f2232200e9119
Justin Nel <dogbertuk2000@gmail.com>
2018-10-01 21:07:17 +0200

    Corrected path.join usage
    path.join will join all arguments passed into it, so no need to concat them yourself.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 var path = require('path')
 
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(3000, () => console.log('rose has set up a server, bitches'))
